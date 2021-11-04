8273cc9a6b7cb214946e05d5d4e3077f4e3dd35e
coding <coding@coding.net>
2021-11-04 08:09:10 +0000

    提交 ...

--- src/main/java/info/matsumana/Application.java
+++ src/main/java/info/matsumana/Application.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Jsr330ScopeMetadataResolver;
 public class Application {
     public static void main(String[] args) {
         System.out.println("commit");
+        
         SpringApplication.run(Application.class, args);
     }
 }
